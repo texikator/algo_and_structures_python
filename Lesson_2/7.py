8ceca1814a6175df292953a61576d691b9386112
texikator <wiktot@mail.ru>
2019-05-25 14:59:57 +0300

    Доделано дз второго урока. Отстаю на одно занятие (

--- Lesson_2/7.py
+++ Lesson_2/7.py
@@ -3,3 +3,29 @@
 натуральных чисел выполняется равенство: 1+2+...+n = n(n+1)/2,
  где n - любое натуральное число.
 """
+
+
+def left_part(n, iteration=0, summ=0):
+
+    iteration += 1
+    summ += iteration
+
+    #print(iteration, summ)
+    if iteration == n:
+        return summ
+    else:
+        return left_part(n, iteration, summ)
+
+
+n = int(input('Введите значение n'))
+left = left_part(n)
+right = n*(n+1)/2
+
+
+if left == right:
+    print('Равенство верно')
+else:
+    print('Равенство неверно')
+
+print(f'Значение левой части равно {left}')
+print(f'Значение правой части равно {right} ')
--- Lesson_2/9.py
+++ Lesson_2/9.py
@@ -2,3 +2,32 @@
 9. Среди натуральных чисел, которые были введены, найти
 наибольшее по сумме цифр. Вывести на экран это число и сумму его цифр.
 """
+
+
+def calc(number, length=1, summ=0):
+
+    if number > 0:
+        length = len(str(number))
+        deci = 10 ** (length - 1)
+        digit = number // deci
+        summ += digit
+        number %= deci
+        
+        return calc(number, length, summ)
+    else:
+        return summ
+
+
+numbers = input('Введите числа, разделенные запятой: ')
+
+max_summ = 0
+
+for i in numbers.split(','):
+    summ = calc(int(i))
+
+    if summ > max_summ:
+        max = i
+        max_summ = summ
+
+print(f'Максимальная сумма: {max_summ}, число: {max}')
+
