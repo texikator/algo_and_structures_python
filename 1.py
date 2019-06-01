67c6b7e5a8643968abf9370fe42fa4af4aacbcc8
texikator <wiktot@mail.ru>
2019-06-01 09:49:32 +0300

    ДЗ 4

--- 1.py
+++ 1.py
@@ -1,25 +1,93 @@
-# 1.	В диапазоне натуральных чисел от 2 до 99 определить,
-# сколько из них кратны каждому из чисел в диапазоне от 2 до 9.
+"""
+1. Проанализировать скорость и сложность одного любого алгоритма,
+разработанных в рамках домашнего задания первых трех уроков.
+Примечание: попробуйте написать несколько реализаций алгоритма и сравнить их.
+"""
+import cProfile, timeit, json
 
-orders = [int(i) for i in range(2, 10)]
-numbers = [int(i) for i in range(2, 100)]
+number = 345737845309495830672987654345678906856790589534958094313264833294052389457089437593487590347509283457034298750348276585931046813463160315965506304589345143889751093487509314875893147508931470893475984317095874510873487658347083175834058475
+number = 13
+number = 234
+number = 2345
+#number = int(input('Введите число'))
 
-multi = [0 for i in range(2,10)]
+
+def get_deci(num):
+    i = 1
+    while num // i >= 1:
+        i *= 10
+    return i/10
 
 
-for order in orders:
-    print(order)
-    for number in numbers:
-        #print(number, order)
-        if number % order == 0:
-            #print(order)
-            multi[order-2] += 1
-#print(multi)
+def recursion_1(num=number, new_number=''):
+    deci = get_deci(num)
+    digit = int(num // deci)
+    if deci > 1:
+        return recursion_1(num % deci, f'{digit}{new_number}')
+    if deci == 1:
+        return f'{digit}{new_number}'
 
-output = f'Числа в интервале 2..9 и количества кратных им чисел: '
-for i in multi:
-    print(i)
-    output += f'{i+2}-{i}, '
+
+def recursion_2(num=number, new_number=''):
+    deci = 10 ** (len(str(num)) - 1)
+
+    digit = str(num // deci)
+    if deci > 1:
+        return recursion_2(num % deci, f'{digit}{new_number}')
+    else:
+        return f'{digit}{new_number}'
+
+
+def use_list(number=number):
+    str_number = str(number)
+    result = []
+    for i in range(len(str_number)):
+        result.insert(0, str_number[i])
     
-print(output)
+    return ('').join(result)
 
+def use_list_2(number=number):
+    str_number = str(number)
+    result = []
+    for i in str_number:
+        result.insert(0,i)
+    return result
+
+def simple(number=number):
+    str_num = str(number)
+    return str_num[::-1]
+
+def main():
+
+    for i in range(100):
+        recursion_1(number)
+        recursion_2(number)
+        use_list(number)
+        simple(number)
+
+cProfile.run('main()')
+
+
+statistics = []
+for i in range(10):
+
+    number = int(f'{str(number)}{str(i)}')
+    data = {}
+    # print(f"recursion_1: {timeit.timeit('recursion_1()', setup='from __main__ import recursion_1', number=1000)}")
+    # print(f"recursion_2: {timeit.timeit('recursion_1', setup='from __main__ import recursion_1', number=10000)}")
+    # print(f"use_list: {timeit.timeit('use_list()', setup='from __main__ import use_list', number=10000)}")
+    # print(f"use_list_2: {timeit.timeit('use_list_2', setup='from __main__ import use_list_2', number=10000)}")
+    # print(f"simple: {timeit.timeit('simple()', setup='from __main__ import simple', number=10000)}")
+    data['number'] = number
+    data['recursion_1'] = timeit.timeit('recursion_1()', setup='from __main__ import recursion_1', number=1000)
+    data['recursion_2'] = timeit.timeit('recursion_2', setup='from __main__ import recursion_2', number=10000)
+    data['use_list'] = timeit.timeit('use_list()', setup='from __main__ import use_list', number=10000)
+    data['use_list_2'] = timeit.timeit('use_list_2', setup='from __main__ import use_list_2', number=10000)
+    data['simple'] = timeit.timeit('simple()', setup='from __main__ import simple', number=10000)
+
+    statistics.append(data)
+
+print(json.dumps(statistics, indent=4))
+
+
+# Не ожидал что штатный метод [::-1] настолько проигрывает рекурсии в варианте 2
--- 2.py
+++ 2.py
@@ -1,18 +1,94 @@
 """
-2.	Во втором массиве сохранить индексы четных элементов первого массива.
-Например, если дан массив со значениями 8, 3, 15, 6, 4, 2, то во второй массив
-надо заполнить значениями 1, 4, 5, 6
-(или 0, 3, 4, 5 - если индексация начинается с нуля),
-т.к. именно в этих позициях первого массива стоят четные числа.
+2. Написать два алгоритма нахождения i-го по счёту простого числа.
+Без использования «Решета Эратосфена»;
+Используя алгоритм «Решето Эратосфена»
 """
 
-import random
-length = int(input('ВВедите длинну массива: '))
-first = [random.randrange(0, 100) for i in range(length)]
-second = []
-print(f'Исходный массив: {first}')
-for i in range(length):
-    if first[i] % 2 == 0:
-        second.append(i)
+import math, timeit, sys
 
-print(f'Позиции четных элементов: {second}')
+sys.setrecursionlimit(100000)
+# #без решета, рекурсия
+# def find_simple(num_count=10, finded=[], number=3):
+#     #print('N ',number)
+#     for i in range(2, int(math.sqrt(number))+1):
+#         #print('i ', i)
+#         if number % i == 0:
+#             break
+#     else:
+#          finded.append(number)
+#          print(number, end=" ")
+#          #print(finded)
+#          #print("sdfsdf", len(finded))
+#     number += 2         
+
+#     if len(finded) == num_count:
+#         return finded
+#     else:
+#         return find_simple(num_count, finded, number)
+
+
+
+#без решета, без рекурсии
+def get_simple(num_count=100, number=2):
+    finded = []
+    simple_count = 0
+    while simple_count < num_count:
+        
+        div = 2
+        
+        while 2 <= div <= int(math.sqrt(number))+1:
+
+            if number % div == 0 and number != div:
+                break
+            div += 1
+            
+        else:
+            finded.append(number)
+            simple_count += 1
+     
+        number += 1
+    return finded
+
+
+
+#num_count = int(input('Введите номер числа: '))
+num_count  = 500
+simple_numbers = get_simple(num_count)
+
+print(f' Метод 1: заданное число: {simple_numbers[-1]} ')
+print(f'Метод 1: Список простых чисел: {simple_numbers}')
+
+
+n = int(input('Введите границу вычисление'))
+
+def eratos(n=800):
+    finded = []
+    a = [i for i in range(1,n+1)]
+    a[0] = 0
+
+    number = 1
+    step =1 
+    while number < n:
+        
+        if a[number] !=0:
+            finded.append(a[number])    
+            step = a[number]
+            #print(f'число в списке {a[number]} ,шаг {step}')
+            for div in range(a[number] ** 2, n+1, step):
+                a[div-1] = 0
+            
+        number += 1
+        
+#    for i in a:
+#        if a > 0:
+#            finded.append(a)
+    return finded
+
+print(print(f'Метод 2: Список простых чисел: {eratos(n)}'))
+
+
+print(timeit.timeit('get_simple()', setup='from __main__ import get_simple', number=100))
+print(timeit.timeit('eratos()', setup='from __main__ import eratos', number=100))
+
+# 0.17245420000000067
+# 0.04399160000000002
